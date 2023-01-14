dcaaab49a3408bf0dbdac6757abd4e4fc9659580
pol-dev-shinroo <manus.dream.language@gmail.com>
2023-01-14 20:30:57 +0900

    sub menu done

--- src/partials/header/SubHeader.tsx
+++ src/partials/header/SubHeader.tsx
@@ -17,80 +17,76 @@ const SubHeader = () => {
           framerExit={{ y: -50 }}
           framertransition={{ type: "spring", stiffness: 300, damping: 30 }}
           zIndex="z-10"
-          className="border border-red-700 w-full"
+          lightClasses={["bg-blue-100", "bg-opacity-70"]}
+          darkClasses={["dark:bg-zinc-700", "dark:bg-opacity-70"]}
+          className="w-full shadow shadow-gray-500"
         >
           <Flex
             align="center"
             justify="between"
             direction="row"
-            className="w-1/3 p-2.5 border border-green-700"
+            className="w-1/3 p-2.5 pt-4"
           >
-            <Flex
-              align="center"
-              justify="center"
-              className="w-4/5 border border-green-700"
-            >
+            <Flex align="center" justify="center" className="w-4/5">
               <FramerBtn
                 onClick={() => handleSubActiveIndex(0)}
                 whileHover={hover}
                 whileTap={tap}
                 lightClasses={
-                  subActiveIndex === 0 ? ["text-slate-900"] : ["text-gray-400"]
+                  subActiveIndex === 0
+                    ? ["text-slate-900", "hover:bg-green-100"]
+                    : ["text-gray-400", "hover:bg-green-100"]
                 }
                 darkClasses={
                   subActiveIndex === 0
-                    ? ["dark:text-slate-50"]
-                    : ["dark:text-gray-400"]
+                    ? ["dark:text-slate-50", "dark:hover:bg-sky-300"]
+                    : ["dark:text-gray-400", "dark:hover:bg-sky-300"]
                 }
-                className={`w-4/5 border border-purple-700 text-lg ${
+                className={`w-4/5 bg-opacity-70 text-lg rounded-lg ${
                   subActiveIndex === 0 && `font-bold `
                 } `}
               >
                 Month
               </FramerBtn>
             </Flex>
-            <Flex
-              align="center"
-              justify="center"
-              className="w-4/5 border border-green-700"
-            >
+            <Flex align="center" justify="center" className="w-4/5 ">
               <FramerBtn
                 onClick={() => handleSubActiveIndex(1)}
                 whileHover={hover}
                 whileTap={tap}
                 lightClasses={
-                  subActiveIndex === 1 ? ["text-slate-900"] : ["text-gray-400"]
+                  subActiveIndex === 1
+                    ? ["text-slate-900", "hover:bg-green-100"]
+                    : ["text-gray-400", "hover:bg-green-100"]
                 }
                 darkClasses={
                   subActiveIndex === 1
-                    ? ["dark:text-slate-50"]
-                    : ["dark:text-gray-400"]
+                    ? ["dark:text-slate-50", "dark:hover:bg-sky-300"]
+                    : ["dark:text-gray-400", "dark:hover:bg-sky-300"]
                 }
-                className={`w-4/5 border border-purple-700 text-lg ${
+                className={`w-4/5 bg-opacity-70 text-lg rounded-lg ${
                   subActiveIndex === 1 && `font-bold `
                 } `}
               >
                 Week
               </FramerBtn>
             </Flex>
-            <Flex
-              align="center"
-              justify="center"
-              className="w-4/5 border border-green-700"
-            >
+            <Flex align="center" justify="center" className="w-4/5 ">
               <FramerBtn
                 onClick={() => handleSubActiveIndex(2)}
                 whileHover={hover}
                 whileTap={tap}
                 lightClasses={
-                  subActiveIndex === 2 ? ["text-slate-900"] : ["text-gray-400"]
+                  subActiveIndex === 2
+                    ? ["text-slate-900", "hover:bg-green-100"]
+                    : ["text-gray-400", "hover:bg-green-100"]
                 }
                 darkClasses={
                   subActiveIndex === 2
-                    ? ["dark:text-slate-50"]
-                    : ["dark:text-gray-400"]
+                    ? ["dark:text-slate-50", "dark:hover:bg-sky-300"]
+                    : ["dark:text-gray-400", "dark:hover:bg-sky-300"]
                 }
-                className={`w-4/5 border border-purple-700 text-lg ${
+                className={`w-4/5 bg-opacity-70 text-lg rounded-lg ${
                   subActiveIndex === 2 && `font-bold `
                 } `}
               >
